a49f934cd000f6ea4a253a39abf2653255d27b7c
AdrianQuayle <adrianq@live.com.au>
2024-04-24 06:42:30 +0800

    added user welcome message and instructions

---
+++ wordle.py
@@ -0,0 +1,9 @@
+def welcome():
+    print("""   
+    Welcome to Wordle!
+    You have 6 tries to guess the five-letter word.
+    Type your guess into the console and submit by pressing enter.
+    A yellow tile indicated that you selected the right letter but it's in the wrong spot.
+    A green tile indicates the correct letter in the correct spot.
+    A grey tile indicated the selected letter is not included in the target word.
+    """)
