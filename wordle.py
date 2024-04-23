93ed225d1aa1ea702149b2d917186ebf6ae506be
AdrianQuayle <adrianq@live.com.au>
2024-04-24 06:49:05 +0800

    Added preliminary guess input code

--- wordle.py
+++ wordle.py
@@ -1,3 +1,8 @@
+file = open('word-bank/all_words.txt')
+lines = file.read().splitlines()
+valid_words = lines.copy()
+file.close()
+
 def welcome():
     print("""   
     Welcome to Wordle!
@@ -7,3 +12,21 @@ def welcome():
     A green tile indicates the correct letter in the correct spot.
     A grey tile indicated the selected letter is not included in the target word.
     """)
+
+def guess_input():
+    while True:
+        try:
+            guess = str(input('Guess the word: '))
+            if len(guess) == 5:
+                if guess in valid_words:
+                    print(guess)
+                    break
+                else:
+                    print("Invalid word")
+            else:
+                print("Input must be 5 characters long")
+        except:
+            print("Invalid input")
+
+welcome()
+guess_input()
