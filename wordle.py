c8846facaa37f6dcbcf523286e2a46e720e994cd
AdrianQuayle <adrianq@live.com.au>
2024-04-24 11:33:23 +0800

    Added score_guess function

--- wordle.py
+++ wordle.py
@@ -1,7 +1,8 @@
-file = open('word-bank/all_words.txt')
-lines = file.read().splitlines()
-valid_words = lines.copy()
-file.close()
+import random
+
+ALL_WORDS = 'word-bank/all_words.txt'
+TARGET_WORDS = 'word-bank/target_words.txt'
+GUESSES = 6
 
 def welcome():
     print("""   
@@ -13,10 +14,22 @@ def welcome():
     A grey tile indicated the selected letter is not included in the target word.
     """)
 
+def get_valid_words():
+    file = open(ALL_WORDS, 'r')
+    valid_words = file.read().splitlines()
+    file.close()
+    return valid_words
+
+def get_target_word():
+    file = open(TARGET_WORDS, 'r')
+    words = file.read().splitlines()
+    file.close()
+    return random.choice(words)
+
 def guess_input():
     while True:
         try:
-            guess = str(input('Guess the word: '))
+            guess = str(input("Guess the word: "))
             if len(guess) == 5:
                 if guess in valid_words:
                     print(guess)
@@ -27,6 +40,25 @@ def guess_input():
                 print("Input must be 5 characters long")
         except:
             print("Invalid input")
+    return guess
 
-welcome()
-guess_input()
+def score_guess(guess, target_word):
+    score = []
+    i = 0
+    for char in guess:
+        if char == target_word[i]:
+            score.append(2)
+        elif char in target_word:
+            score.append(1)
+        else:
+            score.append(0)
+        i += 1
+    return tuple(score)
+
+
+valid_words = get_valid_words()
+target_word = get_target_word()
+
+print(get_target_word())
+guess = guess_input()
+print(score_guess(guess, target_word))
